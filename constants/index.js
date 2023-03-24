c2315b6d09ee49509974de70bfdfdf7e07392c83
Captain F Harlock <fred@mbdfinancials.com>
2023-03-24 02:01:42 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,67 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Code analysis',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Machine learning',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Report generation',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Security Risk Score',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Education',
+  },
+];
+
+export const startingFeatures = [
+  'The AI-powered auditor analyses the code of smart contracts for vulnerabilities, errors and issues that may exist.',
+  'The auditor uses machine learning algorithms to identify patterns and trends in the code that could indicate potential issues.',
+  'The auditor generates a report detailing any issues found, and suggestions for how to fix them before deployment on the blockchain.',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'Value',
+    subtitle:
+        'Investors in GPT4X could benefit from the increased security of the Ethereum network and demand for the native token (GPT4X). As more and more organizations adopt blockchain technology, the need for secure and reliable smart contract solutions will increase. By investing in gpt4x.co, investors can capitalize on this trend and potentially see significant returns.',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Adoption',
+    subtitle:
+        'In addition to providing a valuable service to investors, gpt4x.co could lead to more mainstream adoption of blockchain technology. As smart contracts become more secure, organizations and individuals will be more likely to use them for various applications. This could include financial transactions to supply chain management, creating new opportunities for businesses and individuals.',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Revolutionizing Legal Practices: AI-Powered Smart Contract Auditors Transform the Industry',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Meet Your New Best Friend: How AI Smart Contract Auditors Are Saving Time and Reducing Risk',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'The Future of Legal Compliance: Unveiling the Potential of AI Smart Contract Auditors',
+  },
+];
+
+export const socials = [
+
+];
---
+++ sections/About.jsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About GPT4X" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">GPT4X</span> is an AI-powered Solidity Smart Contract Auditor that uses machine learning to analyze code for vulnerabilities, errors, and issues. The state-of-the-art technology generates comprehensive reports that details any issues found, including a description of the problem, the location of the problem in the code, and suggestions for how to fix it.{' '}
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
